eeba93b5c380d0afd633430c379c335b9bbed6c5
sachii <sachin.kv@sustainablecontext.com>
2021-04-28 11:09:38 +0530

    Add project files.

---
+++ ExamSystemApp/MainForm.vb
@@ -0,0 +1,31 @@
+﻿Public Class MainForm
+    Private Sub Menu1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles MainMenu.ItemClicked
+        If e.ClickedItem.Name = "Logout" Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub Master_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles Master.DropDownItemClicked
+        Dim menuText = e.ClickedItem.ToString()
+
+        Select Case (menuText)
+            Case "State"
+                Dim stateForm As New State()
+                stateForm.MdiParent = Me
+                stateForm.Show()
+            Case Else
+
+        End Select
+
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim loginForm As New LoginForm
+        Dim result = loginForm.ShowDialog(Me)
+        Debug.Write(result)
+        If result <> DialogResult.OK Then
+            loginForm.Dispose()
+            Me.Close()
+        End If
+    End Sub
+End Class
---
+++ ExamSystemApp/State.vb
@@ -0,0 +1,9 @@
+﻿Public Class State
+    Private Sub State_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
