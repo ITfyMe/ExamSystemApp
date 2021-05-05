f9b67d84ace8c6f9c533359f9c9f3dd71e7768ff
sachii <sachin.kv@sustainablecontext.com>
2021-05-05 17:16:26 +0530

    commit collegebranch

--- ExamSystemApp/CollegeBranch.Designer.vb
+++ ExamSystemApp/CollegeBranch.Designer.vb
@@ -22,9 +22,54 @@ Partial Class CollegeBranch
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.lbCollege = New System.Windows.Forms.ListBox()
+        Me.grdBranch = New System.Windows.Forms.DataGridView()
+        Me.txtCollege = New System.Windows.Forms.TextBox()
+        CType(Me.grdBranch, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lbCollege
+        '
+        Me.lbCollege.FormattingEnabled = True
+        Me.lbCollege.ItemHeight = 15
+        Me.lbCollege.Location = New System.Drawing.Point(12, 28)
+        Me.lbCollege.Name = "lbCollege"
+        Me.lbCollege.Size = New System.Drawing.Size(121, 394)
+        Me.lbCollege.TabIndex = 0
+        '
+        'grdBranch
+        '
+        Me.grdBranch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdBranch.Location = New System.Drawing.Point(197, 71)
+        Me.grdBranch.Name = "grdBranch"
+        Me.grdBranch.RowTemplate.Height = 25
+        Me.grdBranch.Size = New System.Drawing.Size(559, 351)
+        Me.grdBranch.TabIndex = 1
+        '
+        'txtCollege
+        '
+        Me.txtCollege.Location = New System.Drawing.Point(197, 39)
+        Me.txtCollege.Name = "txtCollege"
+        Me.txtCollege.Size = New System.Drawing.Size(559, 23)
+        Me.txtCollege.TabIndex = 2
+        '
+        'CollegeBranch
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1111, 694)
+        Me.Controls.Add(Me.txtCollege)
+        Me.Controls.Add(Me.grdBranch)
+        Me.Controls.Add(Me.lbCollege)
+        Me.Name = "CollegeBranch"
         Me.Text = "CollegeBranch"
+        CType(Me.grdBranch, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents lbCollege As ListBox
+    Friend WithEvents grdBranch As DataGridView
+    Friend WithEvents txtCollege As TextBox
 End Class
--- ExamSystemApp/CollegeBranch.vb
+++ ExamSystemApp/CollegeBranch.vb
@@ -1,3 +1,39 @@
 ﻿Public Class CollegeBranch
+    Dim ListCollege As List(Of CCollege) = New List(Of CCollege)
+    Dim ListBranch As List(Of CBranch) = New List(Of CBranch)
 
+    Private Sub PopulateBranch()
+        For index As Int16 = 1 To 15
+            ListBranch.Add(New CBranch(index, "Branch" + index.ToString, "B" + index.ToString))
+        Next
+    End Sub
+    Private Sub PopulateCollege()
+        For index As Int16 = 1 To 10
+            ListCollege.Add(New CCollege(index, "College" + index.ToString, "CC" + index.ToString))
+        Next
+    End Sub
+    Private Sub CollegeBranch_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PopulateBranch()
+        PopulateCollege()
+        lbCollege.DisplayMember = "Name"
+        lbCollege.ValueMember = "ID"
+        lbCollege.DataSource = ListCollege
+        grdBranch.DataSource = ListBranch
+
+
+        ' make all columns read only
+        'For Each column In grdBranch.Columns
+        '    column.ReadOnly = True
+        'Next
+
+        ' add one column to shown the check box to select the branches
+        Dim chk As New DataGridViewCheckBoxColumn
+        grdBranch.Columns.Add(chk)
+        chk.HeaderText = "Select"
+        chk.Name = "chk"
+    End Sub
+
+    Private Sub lbCollege_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lbCollege.SelectedIndexChanged
+        txtCollege.Text = ListCollege(lbCollege.SelectedIndex).Name
+    End Sub
 End Class
